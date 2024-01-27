51a1a276891107cc62ddade7e2e7c90a50672886
Developer Localhost <developer@local.host>
2024-01-27 00:46:29 -0500

    Fix images, styles, Update baseUrl.

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,1 +1,3 @@
-export const baseURL = 'https://localhost:3443/';
+// export const baseURL = 'https://localhost:3443/';
+// export const baseURL = 'https://localhost:3443/mixNotesServer/';
+export const baseURL = 'http://localhost:3000/mixNotesServer/';
