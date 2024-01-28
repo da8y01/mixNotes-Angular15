a07c39af1174f6a8a1b4180a84f264b636c863ef
Developer Localhost <developer@local.host>
2024-01-28 13:25:39 -0500

    Fallback to main baseUrl removing mixNotesServer deploy prefix.

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,3 +1,4 @@
 // export const baseURL = 'https://localhost:3443/';
 // export const baseURL = 'https://localhost:3443/mixNotesServer/';
-export const baseURL = 'http://localhost:3000/mixNotesServer/';
+// export const baseURL = 'http://localhost:3000/mixNotesServer/';
+export const baseURL = 'http://localhost:3000/';
